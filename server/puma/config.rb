d90809a43661485c357983f0278df709f0a54262
pbrazdil <p.brazdil@redhat.com>
2013-09-04 12:00:04 +0200

    basic structure added

--- server/puma/config.rb
+++
@@ -1,7 +0,0 @@
-environment 'production'
-daemonize
-pidfile "#{ENV['OPENSHIFT_RUBY_DIR']}run/server.pid"
-state_path "#{ENV['OPENSHIFT_RUBY_DIR']}run/server.state"
-stdout_redirect "#{ENV['OPENSHIFT_RUBY_DIR']}logs/server.log", "#{ENV['OPENSHIFT_RUBY_DIR']}logs/server_error.log"
-threads 0, 16
-bind "tcp://#{ENV['OPENSHIFT_RUBY_IP']}:8080"
