aab41bdc26071290b0e2fd95a6fd200e77e5ae76
pbrazdil <p.brazdil@redhat.com>
2013-09-05 15:30:03 +0200

    restart file added, bin/control changed

--- servers/puma/config.rb
+++ servers/puma/config.rb
@@ -4,4 +4,4 @@ pidfile "#{ENV['OPENSHIFT_RUBY_DIR']}run/server.pid"
 state_path "#{ENV['OPENSHIFT_RUBY_DIR']}run/server.state"
 stdout_redirect "#{ENV['OPENSHIFT_RUBY_DIR']}logs/server.log", "#{ENV['OPENSHIFT_RUBY_DIR']}logs/server_error.log"
 threads 0, 16
-bind "tcp://#{ENV['OPENSHIFT_RUBY_IP']}:8080"
+bind "tcp://#{ENV['OPENSHIFT_RUBY_IP']}:#{ENV['OPENSHIFT_RUBY_PORT']}"
