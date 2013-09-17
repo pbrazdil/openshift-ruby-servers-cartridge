6da3a2580e5ad18d732a16b2c516cf742bb1536c
pbrazdil <p.brazdil@redhat.com>
2013-09-17 12:27:59 +0200

    little refactoring

--- servers/rainbows/config.rb
+++ servers/rainbows/config.rb
@@ -8,7 +8,7 @@ Rainbows! do
 end
 
 listen "#{ENV['OPENSHIFT_RUBY_IP']}:#{ENV['OPENSHIFT_RUBY_PORT']}"
-pid "#{ENV['OPENSHIFT_RUBY_DIR']}run/server.pid"
+pid "#{ENV['OPENSHIFT_SERVER_PID']}"
 timeout 30
-stdout_path "#{ENV['OPENSHIFT_RUBY_DIR']}logs/server.log"
-stderr_path "#{ENV['OPENSHIFT_RUBY_DIR']}logs/server_error.log"
+stdout_path "#{ENV['OPENSHIFT_SERVER_STDOUT_LOG']}"
+stderr_path "#{ENV['OPENSHIFT_SERVER_STDERR_LOG']}"
